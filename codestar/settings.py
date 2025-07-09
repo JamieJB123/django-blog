071368ec5fbd741311869918d425f9ae80474958
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-09 15:07:26 +0100

    Install gunicorn and prepare django app for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)pqw!lg)_g%te!^$s9p4surg3b+y=ycn@!1taz9h6n^kbm+@9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
