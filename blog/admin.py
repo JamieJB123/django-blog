d2896665614522249a6735efaa1696f8b407c777
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-11 14:59:40 +0100

    Install summernote package and use it to improve admin experience in project

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 # import Post model
 from .models import Post, Comment
+# import summernote
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
