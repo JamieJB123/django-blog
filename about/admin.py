6a1c609cff431ccc8e7059c4029deac020fd82b9
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-16 14:45:03 +0100

    Add: collaborate request form to about me section

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # import Post model
-from .models import About
+from .models import About, CollaborateRequest
 # import summernote
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -9,3 +9,15 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+
+# Note: admin.ModelAdmin is the standard way of registering
+#       our model with the admin panel. We do it differently
+#       above because we are supplying Summernote fields.
+#       If you want to customise the admin panel view in your
+#       own projects, then inherit from admin.ModelAdmin like
+#       we do below.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
