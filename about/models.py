5e4dca3c377d7b7c2f810f8dced5e89899189b76
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-07-17 12:48:30 +0100

    Add: Cloudinary image functionality and display to about and post models / templates

--- about/models.py
+++ about/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 # User Model
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
     author = models.OneToOneField(User, on_delete=models.CASCADE, related_name="about_entry")
